4098011070b878525e96dbeef254c38b98f1469f
Igor <igorexat@gmail.com>
2017-08-01 13:19:00 +0300

    [Core] Added ability ro register container in aggregator

--- tests/unit/ioc-aggregator.test.js
+++ tests/unit/ioc-aggregator.test.js
@@ -1,17 +1,56 @@
-// import IoCContainer from '../../lib/ioc-container';
+import IoCContainer from '../../lib/ioc-container';
 import IoCAggregator from '../../lib/ioc-aggregator';
-// import { simpleResourceExample, functionResourceExample, ClassResourceExample } from '../fixtures/resources';
+import { simpleResourceExample /*, functionResourceExample, ClassResourceExample*/ } from '../fixtures/resources';
 
-// const SIMPLE_RESOURCE_NAME = 'config';
+const SIMPLE_RESOURCE_NAME = 'config';
 // const FUNCTION_RESOURCE_NAME = 'modifier';
 // const CLASS_RESOURCE_NAME = 'increment';
 
 describe('IoCAggregator class', function () {
     describe('Constructor', function () {
         it('should create new instance of IoCAggregator', function () {
-            var ioc = new IoCAggregator([]);
+            let ioc = new IoCAggregator();
 
             expect(ioc).to.be.an.instanceof(IoCAggregator);
+        });
+
+        it('should allow to pass containers', function () {
+            let ioc = new IoCAggregator([new IoCContainer(), new IoCAggregator()]);
+
+            expect(ioc).to.be.an.instanceof(IoCAggregator);
+        });
+    });
+
+    describe('register method', function () {
+        it('should allow to add new container as member of aggregator', function () {
+            let aggregator = new IoCAggregator();
+
+            let container = new IoCContainer();
+
+            container.register(simpleResourceExample, SIMPLE_RESOURCE_NAME);
+
+            aggregator.registerContainer(container);
+
+            let simpleResource = aggregator.resolve(SIMPLE_RESOURCE_NAME);
+
+
+            expect(simpleResource).to.equal(simpleResourceExample);
+        });
+
+        it('should allow to add new aggregator as member of aggregator', function () {
+            let aggregator = new IoCAggregator();
+
+            let container = new IoCContainer();
+
+            container.register(simpleResourceExample, SIMPLE_RESOURCE_NAME);
+            
+            let parentAggregator = new IoCAggregator([container]);
+
+            aggregator.registerContainer(parentAggregator);
+
+            let simpleResource = aggregator.resolve(SIMPLE_RESOURCE_NAME);
+
+            expect(simpleResource).to.equal(simpleResourceExample);
         });
     });
 });
